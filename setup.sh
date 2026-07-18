892e0977e318fee8142ae5bbb4b008a09f4b60b7
galpachi <sutonomega@gmail.com>
2026-07-18 13:45:36 +0900

    Add zsh-vi-mode setup

--- setup.sh
+++ setup.sh
@@ -17,3 +17,9 @@ ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 
 mkdir -p ~/.config
 ln -sfn ~/dotfiles/.config/nvim ~/.config/nvim
+
+mkdir -p ~/.zsh
+
+if [ ! -d ~/.zsh/zsh-vi-mode ]; then
+  git clone https://github.com/jeffreytse/zsh-vi-mode.git ~/.zsh/zsh-vi-mode
+fi
